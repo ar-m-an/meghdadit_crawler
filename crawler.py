36e18fa21340053ac65940eb732399a16615103e
Arman <arman.akbari@gmail.com>
2017-06-28 19:57:15 +0430

    initial commit

---
+++ crawler.py
@@ -0,0 +1,14 @@
+def get_source(url):
+    """ Return HTML source of requested url"""
+
+
+def get_subcat(site_url):
+    """ Return list of urls to all subcategories in the site"""
+
+
+def get_subcat_pages(subcat_url):
+    """ Return a url list of subcategory product pages divided by pagination """
+
+
+def get_page_items(page_url):
+    """  Return a list of product url"""
