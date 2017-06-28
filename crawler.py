67f49c1929e0f7a6e7d06c52231a61838768c926
Arman <arman.akbari@gmail.com>
2017-06-28 21:31:18 +0430

    Add get_subcat() function
    This function returns all subcategories in home page.

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import urllib.request
+from bs4 import BeautifulSoup
 
 
 def get_source(url):
@@ -11,7 +12,15 @@ def get_source(url):
 
 def get_subcat(site_url):
     """ Return list of urls to all subcategories in the site"""
+    selector = ".w25p.pb10 > a"             # CSS Selector for Subcategories in page
+    page_source = get_source(site_url)
 
+    # Parse html source DOM
+    html_DOM = BeautifulSoup(page_source, "html.parser")
+    # Save all href attributes in a list
+    subcats = [a.get('href') for a in html_DOM.select(selector)]
+
+    return subcats
 
 def get_subcat_pages(subcat_url):
     """ Return a url list of subcategory product pages divided by pagination """
@@ -25,4 +34,5 @@ if __name__ == "__main__":
 
     site_url = "http://meghdadit.com/"
 
-    print(get_source(site_url))
+    # print(get_source(site_url))
+    print(get_subcat(site_url))
