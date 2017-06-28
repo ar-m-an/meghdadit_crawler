0e5aa0bf7ee68a4c773f4a848f441fe1eafe9f61
Arman <arman.akbari@gmail.com>
2017-06-28 21:10:09 +0430

    Add get_source function

--- crawler.py
+++ crawler.py
@@ -1,5 +1,12 @@
+import urllib.request
+
+
 def get_source(url):
     """ Return HTML source of requested url"""
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        page_source = response.read()
+        return page_source
 
 
 def get_subcat(site_url):
@@ -12,3 +19,10 @@ def get_subcat_pages(subcat_url):
 
 def get_page_items(page_url):
     """  Return a list of product url"""
+
+
+if __name__ == "__main__":
+
+    site_url = "http://meghdadit.com/"
+
+    print(get_source(site_url))
